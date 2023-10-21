efbb7875cd33ed382cd2ee39cc7d12ed11868491
DOSSE Kovi Amen <koviamendosse3@gmail.com>
2023-10-21 21:24:53 +0000

    updates

--- src/pages/sales/create.vue
+++ src/pages/sales/create.vue
@@ -90,139 +90,121 @@
         ></i>
       </div>
     </div>
-    <div
-      class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-3"
-    >
-      <!-- Première carte (1/4 de l'écran sur les écrans larges) -->
+    <form @submit.prevent="submitForm">
+      <div
+        class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-3"
+      >
+        <!-- Première carte (1/4 de l'écran sur les écrans larges) -->
 
-      <div class="lg:col-span-1">
-        <div class="bg-blue-400 p-4">
-          <!-- <div class="mb-4">
-            <label for="selectedProduct" class="block text-white mb-2"
-              >Choisir un produit:
-            </label>
-            <v-select
-              v-model="selectedProduct"
-              class="bg-white border rounded w-full text-gray-700 py-0 focus:outline-none focus:border-blue-500"
-              required
-              :options="productsStore.products"
-              label="name"
-            ></v-select>
+        <div class="lg:col-span-1">
+          <div class="bg-blue-400 p-4">
+            <div class="mb-4">
+              <label class="block text-white mb-2" for="discount"
+                >Remise:</label
+              >
+              <input
+                v-model="formData.discount"
+                :class="{
+                  'bg-gray-200 cursor-not-allowed':
+                    formData.products.length === 0,
+                  'bg-white': formData.products.length !== 0,
+                }"
+                class="border rounded-md py-1 px-3 w-full"
+                type="text"
+                id="discount"
+                name="discount"
+                placeholder="Remise"
+                required
+                :disabled="formData.products.length === 0"
+              />
+            </div>
+            <div class="mb-4">
+              <label class="block text-white mb-2" for="amountPaid"
+                >Montant reçu:</label
+              >
+              <input
+                v-model="formData.amountPaid"
+                :class="{
+                  'bg-gray-200 cursor-not-allowed':
+                    formData.products.length === 0,
+                  'bg-white': formData.products.length !== 0,
+                }"
+                class="border rounded-md py-1 px-3 w-full"
+                type="text"
+                id="amountPaid"
+                name="amountPaid"
+                placeholder="Montant reçu"
+                :disabled="formData.products.length === 0"
+              />
+            </div>
           </div>
-          <div class="mb-4">
-            <label for="selectedProduct" class="block text-white mb-2"
-              >Choisir un client:
-            </label>
-            <v-select
-              v-model="formData.client"
-              class="bg-white border rounded w-full text-gray-700 py-0 focus:outline-none focus:border-blue-500"
-              required
-              :options="clientsStore.clients"
-              label="name"
-            ></v-select>
-          </div> -->
-          <div class="mb-4">
-            <label class="block text-white mb-2" for="discount">Remise:</label>
-            <input
-              v-model="formData.discount"
-              :class="{
-                'bg-gray-200 cursor-not-allowed':
-                  formData.products.length === 0,
-                'bg-white': formData.products.length !== 0,
-              }"
-              class="border rounded-md py-1 px-3 w-full"
-              type="text"
-              id="discount"
-              name="discount"
-              placeholder="Remise"
-              required
-              :disabled="formData.products.length === 0"
-            />
-          </div>
-          <div class="mb-4">
-            <label class="block text-white mb-2" for="amountPaid"
-              >Montant reçu:</label
-            >
-            <input
-              v-model="formData.amountPaid"
-              :class="{
-                'bg-gray-200 cursor-not-allowed':
-                  formData.products.length === 0,
-                'bg-white': formData.products.length !== 0,
-              }"
-              class="border rounded-md py-1 px-3 w-full"
-              type="text"
-              id="amountPaid"
-              name="amountPaid"
-              placeholder="Montant reçu"
-              :disabled="formData.products.length === 0"
-            />
+          <button
+            type="submit"
+            :class="{ 'cursor-not-allowed': formData.products.length === 0 }"
+            class="py-2 p-4 shadow-xl btn-primary my-4 text-white"
+            :disabled="formData.products.length === 0"
+          >
+            <i class="fa-solid fa-save"></i> Enregistrer la vente
+          </button>
+        </div>
+        <!-- Deuxième carte (3/4 de l'écran sur les écrans larges) -->
+        <div class="lg:col-span-3">
+          <!-- Liste des users -->
+          <table
+            class="shadow-lg w-full table-hover bg-black bg-opacity-70 text-white"
+          >
+            <thead>
+              <tr>
+                <th class="px-6 py-3 text-left text-sm">Produit</th>
+                <th class="px-6 py-3 text-left text-sm">Prix unitaire</th>
+                <th class="px-6 py-3 text-left text-sm">Qté</th>
+                <th class="px-6 py-3 text-left text-sm">Montant</th>
+                <th class="px-6 py-3 text-left text-sm">-</th>
+              </tr>
+            </thead>
+            <tbody class="bg-white text-gray-600 divide-y divide-gray-200">
+              <tr v-for="(product, index) in formData.products" :key="index">
+                <td class="px-6 py-4 whitespace-no-wrap">
+                  {{ product.name + "(" + product.stock + ")" }}
+                </td>
+                <td class="px-6 py-4 whitespace-no-wrap">
+                  {{ product.selling_price }}
+                </td>
+
+                <td class="px-6 py-4 whitespace-no-wrap">
+                  <input
+                    type="number"
+                    v-model="formData.products[index].quantity"
+                    class="border border-gray-300 rounded-lg py-1 px-1 block appearance-none leading-normal focus:outline-none focus:ring focus:border-blue-500"
+                    required
+                  />
+                </td>
+                <td class="px-6 py-4 whitespace-no-wrap">
+                  {{
+                    product.selling_price * formData.products[index].quantity
+                  }}
+                </td>
+
+                <td class="flex gap-2 py-4">
+                  <a
+                    class="p-0.5 px-2 text-white bg-red-500 hover:bg-red-600 shadow-xl rounded-sm"
+                    @click="removeProduct(index)"
+                  >
+                    <i class="fa-regular fa-trash-can"></i>
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div
+            v-if="formData.products.length == 0"
+            class="bg-white w-full p-4 text-center"
+          >
+            Aucun produit sélectionné
           </div>
         </div>
-        <button
-          type="submit"
-          :class="{ 'cursor-not-allowed': formData.products.length === 0 }"
-          class="py-2 p-4 shadow-xl btn-primary my-4 text-white"
-          :disabled="formData.products.length === 0"
-        >
-          <i class="fa-solid fa-save"></i> Enregistrer la vente
-        </button>
       </div>
-      <!-- Deuxième carte (3/4 de l'écran sur les écrans larges) -->
-      <div class="lg:col-span-3">
-        <!-- Liste des users -->
-        <table
-          class="shadow-lg w-full table-hover bg-black bg-opacity-70 text-white"
-        >
-          <thead>
-            <tr>
-              <th class="px-6 py-3 text-left text-sm">Produit</th>
-              <th class="px-6 py-3 text-left text-sm">Prix unitaire</th>
-              <th class="px-6 py-3 text-left text-sm">Qté</th>
-              <th class="px-6 py-3 text-left text-sm">Montant</th>
-              <th class="px-6 py-3 text-left text-sm">-</th>
-            </tr>
-          </thead>
-          <tbody class="bg-white text-gray-600 divide-y divide-gray-200">
-            <tr v-for="(product, index) in formData.products" :key="index">
-              <td class="px-6 py-4 whitespace-no-wrap">
-                {{ product.name + "(" + product.stock + ")" }}
-              </td>
-              <td class="px-6 py-4 whitespace-no-wrap">
-                {{ product.selling_price }}
-              </td>
-
-              <td class="px-6 py-4 whitespace-no-wrap">
-                <input
-                  type="number"
-                  v-model="formData.products[index].quantity"
-                  class="border border-gray-300 rounded-lg py-1 px-1 block appearance-none leading-normal focus:outline-none focus:ring focus:border-blue-500"
-                  required
-                />
-              </td>
-              <td class="px-6 py-4 whitespace-no-wrap">
-                {{ product.selling_price * formData.products[index].quantity }}
-              </td>
-
-              <td class="flex gap-2 py-4">
-                <a
-                  class="p-0.5 px-2 text-white bg-red-500 hover:bg-red-600 shadow-xl rounded-sm"
-                  @click="removeProduct(index)"
-                >
-                  <i class="fa-regular fa-trash-can"></i>
-                </a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div
-          v-if="formData.products.length == 0"
-          class="bg-white w-full p-4 text-center"
-        >
-          Aucun produit sélectionné
-        </div>
-      </div>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -233,12 +215,15 @@ import vSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
 import AddClientModal from "~/components/actions/clients/AddClientModal.vue";
 
+const loading = ref(false);
 const productsStore = useProductsStore();
 const clientsStore = useClientsStore();
+const salesStore = useSalesStore();
 const showAlert = ref(false);
 const alertMessage = ref("");
 const selectedProduct = ref();
 const isOpenCreate = ref(false);
+const router= useRouter();
 const formData = ref<SaleForm>({
   reference: "",
   client: null,
@@ -335,6 +320,23 @@ function existProduct(productId: string) {
   }
 }
 
+const submitForm = async () => {
+  loading.value = true;
+  await salesStore.postData(formData.value).then((status) => {
+    if (status) {
+      const state={showAlert:'true',alertMessage:"Vente enregistrée avec succès"}
+      router.push({ path: '/sales', query: state })
+
+    }
+    // emit('onClose')
+    // emit('onSuccess', "Catégorie ajoutée avec succès")
+
+    loading.value = false;
+  });
+  setTimeout(() => {
+    loading.value = false;
+  }, 15000);
+};
 definePageMeta({
   layout: "main",
 });
--- src/pages/sales/index.vue
+++ src/pages/sales/index.vue
@@ -22,29 +22,23 @@
       <Datatable v-if="!store.loading">
         <thead>
           <tr>
-            <th class="px-6 py-3 text-left text-sm font-bold">Id</th>
             <th class="px-6 py-3 text-left text-sm font-bold">Référence</th>
             <th class="px-6 py-3 text-left text-sm font-bold">Date</th>
             <th class="px-6 py-3 text-left text-sm font-bold">Montant total</th>
             <th class="px-6 py-3 text-left text-sm font-bold">Remise</th>
             <th class="px-6 py-3 text-left text-sm font-bold">Montant payé</th>
             <th class="px-6 py-3 text-left text-sm font-bold">Reliquat</th>
-            <th class="px-6 py-3 text-left text-sm font-bold">Statut du paiement</th>
-            <th class="px-6 py-3 text-left text-sm font-bold">Client</th>
             <th class="px-6 py-3 text-left text-sm font-bold">Actions</th>
           </tr>
         </thead>
         <tbody class="bg-white text-gray-600 divide-y divide-gray-200">
           <tr v-for="(sale, index) in store.sales" :key="index" :title="`Créé le ${formatDateFrench(sale.createdAt)}\nModifié le ${formatDateFrench(sale.updatedAt)}}`">
-            <td class="px-6 py-4 whitespace-no-wrap">{{ sale.id }}
-            </td>
             <td class="px-6 py-4 whitespace-no-wrap">{{ sale.reference }}</td>
-            <td class="px-6 py-4 whitespace-no-wrap">{{ sale.createdAt }}</td>
+            <td class="px-6 py-4 whitespace-no-wrap">{{ formatDateFrench(sale.createdAt) }}</td>
             <td class="px-6 py-4 whitespace-no-wrap">{{ sale.totalAmount }}</td>
             <td class="px-6 py-4 whitespace-no-wrap">{{ sale.discount }}</td>
             <td class="px-6 py-4 whitespace-no-wrap">{{ sale.amountPaid }}</td>
             <td class="px-6 py-4 whitespace-no-wrap">{{ sale.change }}</td>
-            <td class="px-6 py-4 whitespace-no-wrap">{{ sale.saleDetails.length }}</td>
             <td class="flex gap-2">
               <a class="p-0.5 px-2 text-white  bg-yellow-500 hover:bg-yellow-600 shadow-xl rounded-sm"
                 @click="openModal(sale, 'read')">
@@ -76,32 +70,37 @@
 <style scoped></style>
 
 <script setup lang="ts">
-import AddCategoryModal from '~/components/actions/categories/AddCategoryModal.vue';
 import ViewCategoryModal from '~/components/actions/categories/ViewCategoryModal.vue';
-import EditCategoryModal from '~/components/actions/categories/EditCategoryModal.vue';
 import DeleteCategoryModal from '~/components/actions/categories/DeleteCategoryModal.vue';
-import { Category,Sale } from '~/types';
+import { Sale } from '~/types';
 import { formatDateFrench } from '~/utils/constants';
 import { useSalesStore } from '~/stores/salesStore';
 
 const store = useSalesStore();
-
 const selectedData = ref<Sale>();
-const isOpenCreate = ref(false);
 const isOpenRead = ref(false);
 const isOpenDelete = ref(false);
-const isOpenEdit = ref(false);
 const showAlert = ref(false);
-const alertMessage = ref("");
+const alertMessage =<any>ref("");
 
-const openModal = (data: Category, action: String) => {
+const route = useRoute();
+
+// Vérifiez si le paramètre de requête 'sessionExpired' est présent
+
+onMounted(() => {
+  const showAlert = route.query.showAlert === 'true';
+
+  // Utilisez 'showAlert' pour afficher le message approprié dans votre interface utilisateur
+  if (showAlert &&  route.query.alertMessage) {
+    alertMessage.value= route.query.alertMessage;    
+  }
+})
+const openModal = (data: Sale, action: String) => {
   selectedData.value = data
 
 
   switch (action) {
     case 'read': isOpenRead.value = true;
-      break;
-    case 'edit': isOpenEdit.value = true;
       break;
     case 'delete': isOpenDelete.value = true;
       break;
